8998780e36c556ccf4d45e5eaa40dc9fc77845fb
MOHAMMED YAMEEN MOHAMMED ELYAS <109512433+yamyy123@users.noreply.github.com>
2022-10-01 09:06:32 +0530

    Create T find whether a number is even or not.c

---
+++ T find whether a number is even or not.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+       printf("EVEN");
+    }
+    else
+    {
+       printf("ODD");
+    }
+}
