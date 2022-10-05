ba8a4738aef87cc45e6b466338b1f6a8069a4736
MOHAMMED YAMEEN S M <109512433+yamyy123@users.noreply.github.com>
2022-10-05 10:20:25 +0530

    Create short circuit operator with increment operands.cr

---
+++ short circuit operator with increment operands.cr
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    a=b=10;
+    while(a++<=13||b++<=13)
+    {
+        printf("%d %d\n",a,b);
+    }
+printf("%d %d",a+10,b+10);
+    return 0;
+}
