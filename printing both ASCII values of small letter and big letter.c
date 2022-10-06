80f353abb982f313e2cb979e4bd75d13ec7f13d0
MOHAMMED YAMEEN S M <109512433+yamyy123@users.noreply.github.com>
2022-10-06 21:01:34 +0530

    Create printing both ASCII values of small letter and big letter.c

---
+++ printing both ASCII values of small letter and big letter.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  char a =65;
+  char b = 97;
+ while(a<=90)
+ {
+    printf("%c ",a);
+  a++;
+ }
+ printf("\n");
+ while(b<=122)
+ {
+    printf("%c ",b);
+  b++;
+ }
+ return 0;
+}
